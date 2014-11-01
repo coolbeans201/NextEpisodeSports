f04566d4c1c47f5e0d8c8c96055daab18094f309
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 10:12:42 -0400

    Create StatsRetrievalResult.php
    Stats retrieval result page

---
+++ StatsRetrievalResult.php
@@ -0,0 +1,5 @@
+#!/usr/local/bin/php
+<html><head><title>Stats Retrieval Result</title></head>
+<body bgcolor="#9ACD32">
+<p align = "center"><b><font size = 6>Result</font></b></p>
+<hr noshade size=5 width="100%">
