3c806014d0de8b0858d0d4a15e998bbba4d541df
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 18:38:12 -0400

    Update SortQuery.php

--- SortQuery.php
+++ SortQuery.php
@@ -1,8 +1,108 @@
 #!/usr/local/bin/php
-<html><head><title>Sort Query</title></head>
-<style>
-	html {overflow-y: scroll}
-</style>
-<body bgcolor="#D2691E">
-<p align = "center"><b><font size = 6>To Sort, Do The Following:</font></b></p>
+<html>
+
+<head>
+	<title>Sort Query</title>
+	<meta name="keywords" content="Next Episode, sort query">
+	<meta name="description" content="Next Episode Sports Database">
+	<meta name="author" content="Derek Hua, Matt Weingarten, Xin He, Jesse Chau">
+	<meta name="copyright" content="Copyright &copy 2014, All Rights Reserved">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light|Rock+Salt">
+	
+    <style>
+        <style style="text/css">
+        html {overflow-y: scroll}
+        
+		h1 {
+			font-family: 'Rock Salt', sans-serif;
+			font-size: 60px;
+			font-weight: normal;
+			color:#000000;
+			text-align: center;
+		}
+		
+		body {
+			font-family: 'Open Sans', sans-serif;
+			font-size: 24px;
+			background-color: #F9F4E1;
+     	}
+		
+		.chooseoperation {
+			text-align:center;
+		}
+		
+        .buttoncenter {
+            text-align:center;
+        }
+        
+        
+		nav ul ul {
+			display: none;
+		}
+		nav ul li:hover > ul {
+			display: block;
+		}
+		
+		nav ul {
+			background: #efefef; 
+			background: linear-gradient(top, #efefef 0%, #bbbbbb 100%);  
+			background: -moz-linear-gradient(top, #efefef 0%, #bbbbbb 100%); 
+			background: -webkit-linear-gradient(top, #efefef 0%,#bbbbbb 100%); 
+			box-shadow: 0px 0px 9px rgba(0,0,0,0.15);
+			padding: 0 20px;
+			border-radius: 10px;  
+			list-style: none;
+			position: relative;
+			display: inline-table;
+		}
+		nav ul:after {
+			content: ""; clear: both; display: block;
+		}
+		
+		nav ul li {
+			float: left;
+		}
+		nav ul li:hover {
+			background: #4b545f;
+			background: linear-gradient(top, #4f5964 0%, #5f6975 40%);
+			background: -moz-linear-gradient(top, #4f5964 0%, #5f6975 40%);
+			background: -webkit-linear-gradient(top, #4f5964 0%,#5f6975 40%);
+		}
+		
+		nav ul li:hover a {
+			color: #fff;
+		}
+		
+		nav ul li a {
+			display: block; padding: 25px 40px;
+			color: #757575; text-decoration: none;
+		}
+		
+		nav ul ul {
+			background: #5f6975; border-radius: 0px; padding: 0;
+			position: absolute; top: 100%;
+		}
+		nav ul ul li {
+			float: none; 
+			border-top: 1px solid #6b727c;
+			border-bottom: 1px solid #575f6a;
+			position: relative;
+		}
+		
+		nav ul ul li a {
+			padding: 15px 40px;
+			color: #fff;
+		}	
+		
+		nav ul ul li a:hover {
+			background: #4b545f;
+		}
+		
+		nav ul ul ul {
+			position: absolute; left: 100%; top:0;
+		}
+    </style>
+</head>
+<body background="squared_metal.png">
+<h1>Sort Query</h1>
 <hr noshade size=5 width="100%">
