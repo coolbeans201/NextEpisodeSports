cd60d72232a82a1e198d62c7a5e7c1e08e2b964b
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 15:34:37 -0400

    Update HomePage.php

--- HomePage.php
+++ HomePage.php
@@ -1,9 +1,7 @@
 #!/usr/local/bin/php
 <html>
-
 <head>
     <title>NExt Episode</title>
-
     <style>
         <style style="text/css">
         html {overflow-y: scroll}
@@ -12,23 +10,12 @@
             text-align:center;
 	    font-size:48px;
         }
-        
-        
     </style>
-
-
 </head>
-
-
-
 <body bgcolor="#FFA500">
-
 <p align = "center"><b><font size = 6>Welcome to Next Episode Sports!</font></b></p>
-
 <hr noshade size=5 width="100%">
-
 <p align = "center"><font size = 4>Choose an operation:</font></p>
-
 <div class="buttoncenter">
 <select name = "operationChoice" size = "1" required>
   <option value="StatsRetrieval">Retrieve Stats</option>
@@ -36,8 +23,6 @@
   <option value="Sort">Sort by Statistic</option>
   <option value="StatQueries">Statistical Queries</option>
 </select>
-
-<a href="http://cise.ufl.edu/~weingart"> <button type = "submit" style = "background-color: lightgreen">Shrek</button> </a> 
+<a href="http://cise.ufl.edu/~weingart"> <button type = "submit" style = "background-color: lightgreen">Go</button> </a> 
 </div>
-
 </body></html>
