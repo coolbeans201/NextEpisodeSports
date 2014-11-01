72b7fcb4aae1a1ab42f3e7dc3cc36bfa1db9ebac
Derek Hua <dhua20@yahoo.com>
2014-11-01 16:41:49 -0400

    Update HomePage.php

--- HomePage.php
+++ HomePage.php
@@ -1,29 +1,165 @@
 #!/usr/local/bin/php
 <html>
+
 <head>
-    <title>Next Episode</title>
+	<title>NExt Episode</title>
+	<meta name="keywords" content="Next Episode, homepage">
+	<meta name="description" content="Next Episode Sports Database">
+	<meta name="author" content="Derek Hua, Matt Weingarten, Xin He, Jesse Chau">
+	<meta name="copyright" content="Copyright &copy 2014, All Rights Reserved">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light|Rock+Salt">
+	
     <style>
         <style style="text/css">
         html {overflow-y: scroll}
+        
+		h1 {
+			font-family: 'Rock Salt', sans-serif;
+			font-size: 60px;
+			font-weight: normal;
+			color:#000000;
+			text-align: center;
+		}
+		
+		body {
+			font-family: 'Open Sans', sans-serif;
+			font-size: 24px;
+			background-color: #F9F4E1;
+     	}
+		
+		.chooseoperation {
+			text-align:center;
+		}
+		
         .buttoncenter {
             text-align:center;
         }
+        
+        
+		nav ul ul {
+			display: none;
+		}
+
+		nav ul li:hover > ul {
+			display: block;
+		}
+		
+		nav ul {
+			background: #efefef; 
+			background: linear-gradient(top, #efefef 0%, #bbbbbb 100%);  
+			background: -moz-linear-gradient(top, #efefef 0%, #bbbbbb 100%); 
+			background: -webkit-linear-gradient(top, #efefef 0%,#bbbbbb 100%); 
+			box-shadow: 0px 0px 9px rgba(0,0,0,0.15);
+			padding: 0 20px;
+			border-radius: 10px;  
+			list-style: none;
+			position: relative;
+			display: inline-table;
+		}
+		nav ul:after {
+			content: ""; clear: both; display: block;
+		}
+		
+		nav ul li {
+			float: left;
+		}
+		nav ul li:hover {
+			background: #4b545f;
+			background: linear-gradient(top, #4f5964 0%, #5f6975 40%);
+			background: -moz-linear-gradient(top, #4f5964 0%, #5f6975 40%);
+			background: -webkit-linear-gradient(top, #4f5964 0%,#5f6975 40%);
+		}
+		
+		nav ul li:hover a {
+			color: #fff;
+		}
+		
+		nav ul li a {
+			display: block; padding: 25px 40px;
+			color: #757575; text-decoration: none;
+		}
+		
+		nav ul ul {
+			background: #5f6975; border-radius: 0px; padding: 0;
+			position: absolute; top: 100%;
+		}
+		nav ul ul li {
+			float: none; 
+			border-top: 1px solid #6b727c;
+			border-bottom: 1px solid #575f6a;
+			position: relative;
+		}
+		
+		nav ul ul li a {
+			padding: 15px 40px;
+			color: #fff;
+		}	
+		
+		nav ul ul li a:hover {
+			background: #4b545f;
+		}
+		
+		nav ul ul ul {
+			position: absolute; left: 100%; top:0;
+		}
     </style>
+
 </head>
-<body bgcolor="#FFA500">
-<h1 align = "center"><b><font size = 6>Welcome to Next Episode Sports!</font></b></h1>
+
+<body background="squared_metal.png">
+
+<h1> Welcome to Next Episode Sports! </h1>
+
 <hr noshade size=5 width="100%">
-<p align = "center"><font size = 4>Choose an operation:</font></p>
+
+<nav class="buttoncenter">
+	<ul>
+		<li><a href="HomePage.php">Home</a></li>
+		<li><a href="#">Functions</a>
+			<ul>
+				<li><a href="StatsRetrievalQuery.php">Retrieve</a></li>
+				<li><a href="CompareQuery.php">Compare</a></li>
+				<li><a href="SortQuery.php">Sort</a>
+					<ul>
+						<li><a href="#">shit</a></li>
+						<li><a href="#">fuck</a></li>
+					</ul>
+				</li>
+				<li><a href="StatsQuery.php">Statistical Queries</a></li>
+			</ul>
+		</li>
+		<li><a href="#">About</a>
+			
+		</li>
+		<li><a href="#">Inspiration</a></li>
+	</ul>
+</nav>
+
+
+<div class="">Choose an operation:</div>
+
 <div class="buttoncenter">
-<form action = "process.php" method = "post">
-<select name = "operationChoice" size = "1" required>
-  <option value="StatsRetrieval">Retrieve Stats</option>
-  <option value="ComparePlayers">Compare</option>
-  <option value="Sort">Sort by Statistic</option>
-  <option value="StatQueries">Statistical Queries</option>
-</select>
-<input type = "submit" style = "background-color: lightgreen"/>
-</form>
+	<form action = "StatsRetrieval.php" method = "post">
+		<select name = "operationChoice" size = "1" required>
+			<option value="StatsRetrieval.php">Retrieve Stats</option>
+			<option value="ComparePlayers.php">Compare</option>
+			<option value="Sort.php">Sort by Statistic</option>
+			<option value="StatQueries.php">Statistical Queries</option>
+		</select>
+	<input type = "submit" style = "background-color: lightgreen"/>
+	</form>
+	
 <?php $selectedValue = $_POST['operationChoice']; echo $selectedValue; ?> 
 </div>
+
 </body></html>
+
+
+
+
+
+
+
+
+
+
