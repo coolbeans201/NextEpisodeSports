e9316f09df654f0751e5e6fa9e608084046967a7
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 10:13:20 -0400

    Create CompareQuery.php
    Compare query page

---
+++ CompareQuery.php
@@ -0,0 +1,5 @@
+#!/usr/local/bin/php
+<html><head><title>Compare Query</title></head>
+<body bgcolor="#FA8072">
+<p align = "center"><b><font size = 6>To Compare, Do The Following:</font></b></p>
+<hr noshade size=5 width="100%">
