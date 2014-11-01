39a28946c4b3fc29d8a7176495efc4c5ff31963d
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 10:22:16 -0400

    Create SortQuery.php
    Sort query page

---
+++ SortQuery.php
@@ -0,0 +1,5 @@
+#!/usr/local/bin/php
+<html><head><title>Sort Query</title></head>
+<body bgcolor="#D2691E">
+<p align = "center"><b><font size = 6>To Sort, Do The Following:</font></b></p>
+<hr noshade size=5 width="100%">
