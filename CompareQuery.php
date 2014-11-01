55f57eb14eb6919d53b8f764de8eddea3ba7dfb0
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 12:24:43 -0400

    Update CompareQuery.php
    Vertical scrollbar always appears

--- CompareQuery.php
+++ CompareQuery.php
@@ -1,5 +1,8 @@
 #!/usr/local/bin/php
 <html><head><title>Compare Query</title></head>
+<style>
+	html {overflow-y: scroll}
+</style>
 <body bgcolor="#FA8072">
 <p align = "center"><b><font size = 6>To Compare, Do The Following:</font></b></p>
 <hr noshade size=5 width="100%">
