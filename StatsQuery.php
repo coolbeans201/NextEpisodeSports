d4fa5abc4afac25a41ec9c94871b1ce38097a3b0
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 12:26:35 -0400

    Update StatsQuery.php
    Vertical scrollbar always appears

--- StatsQuery.php
+++ StatsQuery.php
@@ -1,5 +1,8 @@
 #!/usr/local/bin/php
 <html><head><title>Stats Query</title></head>
+<style>
+	html {overflow-y: scroll}
+</style>
 <body bgcolor="#F4A460">
 <p align = "center"><b><font size = 6>To Perform Statistical Queries, Do The Following:</font></b></p>
 <hr noshade size=5 width="100%">
