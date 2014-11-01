b1c0e6993de51f5b491f8b1c22c4f08d58cb78e5
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 12:25:44 -0400

    Update SortQuery.php
    Vertical scrollbar always appears

--- SortQuery.php
+++ SortQuery.php
@@ -1,5 +1,8 @@
 #!/usr/local/bin/php
 <html><head><title>Sort Query</title></head>
+<style>
+	html {overflow-y: scroll}
+</style>
 <body bgcolor="#D2691E">
 <p align = "center"><b><font size = 6>To Sort, Do The Following:</font></b></p>
 <hr noshade size=5 width="100%">
