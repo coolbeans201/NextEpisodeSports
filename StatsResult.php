25e53239a4ec8c528682b4b79a808ae6f045f1cf
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 10:27:45 -0400

    Create StatsResult.php
    Stats result page

---
+++ StatsResult.php
@@ -0,0 +1,5 @@
+#!/usr/local/bin/php
+<html><head><title>Stats Result</title></head>
+<body bgcolor="#C0C0C0">
+<p align = "center"><b><font size = 6>Result</font></b></p>
+<hr noshade size=5 width="100%">
