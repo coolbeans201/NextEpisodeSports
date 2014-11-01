3e439ed2cdea9e13cf1f4aa750be88c62764512d
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 12:25:16 -0400

    Update CompareResult.php
    Vertical scrollbar always appears

--- CompareResult.php
+++ CompareResult.php
@@ -1,5 +1,8 @@
 #!/usr/local/bin/php
 <html><head><title>Compare Result</title></head>
+<style>
+	html {overflow-y: scroll}
+</style>
 <body bgcolor="#DAA520">
 <p align = "center"><b><font size = 6>Result</font></b></p>
 <hr noshade size=5 width="100%">
