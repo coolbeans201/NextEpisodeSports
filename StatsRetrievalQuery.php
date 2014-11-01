8702235d14b3b657a71214c17104ce43ea298b49
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 10:11:37 -0400

    Create StatsRetrievalQuery.php
    Stats retrieval query page

---
+++ StatsRetrievalQuery.php
@@ -0,0 +1,5 @@
+#!/usr/local/bin/php
+<html><head><title>Stats Retrieval Query</title></head>
+<body bgcolor="#1E90FF">
+<p align = "center"><b><font size = 6>To Retrieve Stats, Do The Following:</font></b></p>
+<hr noshade size=5 width="100%">
