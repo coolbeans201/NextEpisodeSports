6ae458e4ddf39b45134cb6d96a79197a975e9105
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 09:51:21 -0400

    Create HomePage.php
    Created the home page

---
+++ HomePage.php
@@ -0,0 +1,19 @@
+#!/usr/local/bin/php
+<html><head><title>Home Page</title></head>
+<style>
+	select {overflow-y: scroll}
+</style>
+<body bgcolor="#FFA500">
+<p align = "center"><b><font size = 6>Welcome to Next Episode Sports!</font></b></p>
+<hr noshade size=5 width="100%">
+<p align = "center"><font size = 4>Choose an operation:</font></p>
+<select name = "operationChoice" size = "1" required>
+  <option value="StatsRetrieval">Retrieve Stats</option>
+  <option value="ComparePlayers">Compare</option>
+  <option value="Sort">Sort by Statistic</option>
+  <option value="StatQueries">Statistical Queries</option>
+</select>
+<a href="http://cise.ufl.edu/~weingart">
+    <button type = "submit" style = "background-color: lightgreen">Go</button>
+</a>
+</body></html>
