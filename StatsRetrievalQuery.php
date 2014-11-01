9e9c7c7b9a2a19b153876166dfb87993a8ccdee5
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 12:23:32 -0400

    Update StatsRetrievalQuery.php
    Vertical scrollbar always appears

--- StatsRetrievalQuery.php
+++ StatsRetrievalQuery.php
@@ -1,5 +1,8 @@
 #!/usr/local/bin/php
 <html><head><title>Stats Retrieval Query</title></head>
+<style>
+	html {overflow-y: scroll}
+</style>
 <body bgcolor="#1E90FF">
 <p align = "center"><b><font size = 6>To Retrieve Stats, Do The Following:</font></b></p>
 <hr noshade size=5 width="100%">
