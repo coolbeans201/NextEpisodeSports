f3e94ac8387210ead307eb768a3f5b7bfc01818c
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 10:24:29 -0400

    Create SortResult.php
    Sort result page

---
+++ SortResult.php
@@ -0,0 +1,5 @@
+#!/usr/local/bin/php
+<html><head><title>Sort Result</title></head>
+<body bgcolor="#3CB371">
+<p align = "center"><b><font size = 6>Result</font></b></p>
+<hr noshade size=5 width="100%">
