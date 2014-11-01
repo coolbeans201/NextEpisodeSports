110995e02fe692a6c529ecb6af69c0ce93f84437
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 10:26:05 -0400

    Create StatsQuery.php
    Stats query page

---
+++ StatsQuery.php
@@ -0,0 +1,5 @@
+#!/usr/local/bin/php
+<html><head><title>Stats Query</title></head>
+<body bgcolor="#F4A460">
+<p align = "center"><b><font size = 6>To Perform Statistical Queries, Do The Following:</font></b></p>
+<hr noshade size=5 width="100%">
