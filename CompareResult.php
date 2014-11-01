916fc559b371546c9149436607de2852386e6396
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 10:16:23 -0400

    Create CompareResult.php
    Compare result page

---
+++ CompareResult.php
@@ -0,0 +1,5 @@
+#!/usr/local/bin/php
+<html><head><title>Compare Result</title></head>
+<body bgcolor="#DAA520">
+<p align = "center"><b><font size = 6>Result</font></b></p>
+<hr noshade size=5 width="100%">
