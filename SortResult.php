c1d4f7a6ca34110bdaccc2558048e1ee999cfcf6
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 12:26:09 -0400

    Update SortResult.php
    Vertical scrollbar always appears

--- SortResult.php
+++ SortResult.php
@@ -1,5 +1,8 @@
 #!/usr/local/bin/php
 <html><head><title>Sort Result</title></head>
+<style>
+	html {overflow-y: scroll}
+</style>
 <body bgcolor="#3CB371">
 <p align = "center"><b><font size = 6>Result</font></b></p>
 <hr noshade size=5 width="100%">
