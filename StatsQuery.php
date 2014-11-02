6a50fe108c09ee23f223e0fe53a42c0679dbcf11
Matthew Weingarten <matthewben5@aol.com>
2014-11-01 22:45:20 -0400

    Update StatsQuery.php

--- StatsQuery.php
+++ StatsQuery.php
@@ -1,6 +1,4 @@
 #!/usr/local/bin/php
-<!--stats-->
-
 <html>
 
 <head>
@@ -104,35 +102,73 @@
 			position: absolute; left: 100%; top:0;
 		}
     </style>
+<script type="text/javascript">
+var players=[[],
+["Manager","Pitcher","Position Player","Team"],
+["Coach","Player","Team"],
+["Coach","Goalie","Position Player","Team"]
+];
+function PlayerType(idx) {
+var f=document.myForm;
+f.playerType.options.length=null;
+for(i=0; i < players[idx].length; i++) {
+    f.playerType.options[i]=new Option(players[idx][i], i); 
+    }    
+}
+</script>
+<script type = "text/javascript">
+var statistics=[[],["Games","Starts","Atbats","Runs","Hits","Batting average","Doubles", "Triples","Home runs","Slugging","RBI","Stolen bases","Caught stealing","Walks","Strikeouts", "Intentional walks", "Hit by pitch", "OBP", "Sac hits", "Sac flies", "GIDP", "Outs", "Putouts", "Assists", "Errors", "Double plays", "Passed balls", "Wild pitches", "Stolen bases allowed", "Caught stealing allowed", "Fielding percent", "Wins", "Losses", "Winning percentage", "Complete games", "Shutouts", "Saves", "Outs", "Earned runs", "ERA", "Balks", "Batters faced", "Finishes", "H9", "HR9", "BB9", "SO9", "Runs allowed", "Hits allowed", "Home runs allowed", "Walks allowed", "Strikeouts forced"],
+["Wins", "Losses", "Winning percentage", "Postseason wins", "Postseason losses", "Postseason winning percentage", "Minutes", "Points", "Offensive rebounds", "Defensive rebounds", "Rebounds", "Assists", "Steals", "Blocks", "Turnovers", "Fouls", "Field goal attempts", "Field goals made", "Field goal percentage", "Free throw attempts", "Free throws made", "Free throw percentage", "Three point attempts", "Three point percentage", "Points per game", "Rebounds per game", "Assists per game", "Field goals made allowed", "Field goal attempts allowed", "Field goal percentage allowed", "Free throws made allowed", "Free throw attempts allowed", "Three point made allowed", "Three point attempts allowed", "Three point percentage allowed", "Offensive rebounds allowed", "Defensive rebounds allowed", "Rebounds allowed", "Rebounds allowed per game", "Assists allowed", "Assists allowed per game", "Fouls allowed", "Steals allowed", "Turnovers allowed", "Blocks allowed", "Points allowed", "Points allowed per game", "Offensive team rebounds", "Defensive team rebounds", "Home wins", "Home losses", "Home winning percentage", "Away wins", "Away losses", "Away winning percentage", "Neutral wins", "Neutral losses", "Neutral winning percentage", "Conference wins", "Conference losses", "Conference winning percentage", "Division wins", "Division losses", "Division winning percentage", "Games"],
+["Wins", "Losses", "Ties", "Points per game", "Postseason wins", "Postseason losses", "Postseason ties", "Postseason winning percentage", "Games", "Minutes", "Empty net goals", "Shutouts", "Goals allowed", "Shots allowed", "Save percentage", "Shootout wins", "Shootout losses", "Shootout winning percentage", "Shootout shots allowed", "Shootout goals allowed", "Shootout save percentage", "Goals", "Goals per game", "Assists", "Assists per game", "Points", "Points per game", "Penalty minutes", "+/-", "Power play goals", "Power play assists", "Short handed goals", "Short handed assists", "Game winning goals", "Game tying goals", "Shots", "Shot percent", "Shootout shots", "Shootout goals", "Shootout goal deciding goals", "Shootout shot percent", "Overtime losses", "Goal differential", "Bench minutes", "Power play chances", "Short handed goals allowed", "Penalty kill goals", "Penalty kill chances", "Home wins", "Home losses", "Home ties", "Home OT losses", "Home points per game", "Road wins", "Road losses", "Road ties", "Road OT losses", "Road points per game"]];
+function Statistics(idx) {
+var f=document.myForm;
+f.stats.options.length=null;
+for(i=0; i < statistics[idx].length; i++) {
+    f.stats.options[i]=new Option(statistics[idx][i], i); 
+    }    
+}
+</script>
 </head>
 <body background="squared_metal.png">
-	<h1>Compare Query</h1>
-
-	<hr noshade size=5 width="100%">
-	
-	<nav class="buttoncenter">
-		<ul>
-			<li><a href="HomePage.php">Home</a></li>
-			<li><a href="#">Functions</a>
-				<ul>
-					<li><a href="StatsRetrievalQuery.php">Retrieve</a></li>
-					<li><a href="CompareQuery.php">Compare</a></li>
-					<li><a href="SortQuery.php">Sort</a></li>
-					<li><a href="StatsQuery.php">Statistical Queries</a></li>
-				</ul>
-			</li>
-			<li><a href="Team.php">Team</a>			
-			</li>
-			<li><a href="Inspiration.php">Inspiration</a></li>
-		</ul>
-	</nav>	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+<h1>Stats Query</h1>
+<hr noshade size=5 width="100%">
+<form name = "myForm" action = "StatsResult.php" method = "post">
+<div>
+<font size = "4">Select sport: </font>
+<select name = "sport" onChange = "PlayerType(this.selectedIndex); Statistics(this.selectedIndex);">
+	<option value = "selectasport">-Select a Sport-</option>
+	<option value = "baseball">Baseball</option>
+	<option value = "basketball">Basketball</option>
+	<option value = "hockey">Hockey</option>
+</select>
+</div>
+<div>
+<font size = "4">Select player type:</font>
+<select name = "playerType"></select>
+</div>
+<div>
+<font size = "4">Select an operation:</font>
+<select name = "operation">
+	<option value = "average">Average</option>
+	<option value = "min">Minimum</option>
+	<option value = "max">Maximum</option>
+	<option value = "count">Count</option>
+	<option value = "standardDeviation">Standard Deviation</option>
+</select>
+</div>
+<div>
+<font size = "4">Select statistic:</font>
+<select name = "stats"></select>
+</div>
+<div>
+<font size = "4">Select beginning year:</font>
+<select name = "begYear"></select>
+</div>
+<div>
+<font size = "4">Select ending year:</font>
+<select name = "endYear"></select>
+</div>
+<input type = "submit" style = "color: green" value = "Compute">
+</form>
 </body>
+</html>
