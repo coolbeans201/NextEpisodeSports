d6bde673828a4a7959a606b0f9557c172b4059cd
Matthew Weingarten <matthewben5@aol.com>
2014-11-08 07:20:31 -0500

    Update StatsQuery.php

--- StatsQuery.php
+++ StatsQuery.php
@@ -210,6 +210,53 @@ function ajaxFunction(){
 		 ajaxRequest.send(null); 
 }
 </script>
+<script type = "text/javascript">
+function ajaxStats(){
+		 var ajaxRequest;  // The variable that makes Ajax possible!
+			
+		 try{
+		   // Opera 8.0+, Firefox, Safari
+		   ajaxRequest = new XMLHttpRequest();
+		 }catch (e){
+		   // Internet Explorer Browsers
+		   try{
+			  ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+		   }catch (e) {
+			  try{
+				 ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+			  }catch (e){
+				 // Something went wrong
+				 alert("Your browser broke!");
+				 return false;
+			  }
+		   }
+		 }
+		 // Create a function that will receive data 
+		 // sent from the server and will update
+		 // div section in the same page.
+		 ajaxRequest.onreadystatechange = function(){
+		   if(ajaxRequest.readyState == 4){
+			  var ajaxDisplay = document.getElementById('ajaxDiv');
+			  ajaxDisplay.innerHTML = ajaxRequest.responseText;
+		   }
+		 }
+		 // Now get the value from user and pass it to
+		 // server script.
+		 var sport = document.getElementById('sport1').value;
+		 var playertype = document.getElementById('playerType1').value;
+		 var operation = document.getElementById('operation1').value;
+		 var stats = document.getElementById('stats').value;
+		 var begYear = document.getElementById('year1').value;
+		 var endYear = document.getElementById('year2').value;
+		 var queryString = "?sport=" + sport ;
+		 queryString += "&playertype=" + playertype;
+		 queryString += "&operation=" + operation;
+		 queryString += "&stats=" + stats;
+		 queryString += "&begYear=" + begYear;
+		 queryString += "&endYear=" + endYear;
+		 ajaxRequest.open("GET", "getStats.php" + queryString, true);
+		 ajaxRequest.send(null);
+</script>
 </head>
 <body background="squared_metal.png">
 <h1>Stats Query</h1>
@@ -230,7 +277,7 @@ function ajaxFunction(){
 		<li><a href="Inspiration.php">Inspiration</a></li>
 	</ul>
 </nav>
-<form name = "myForm" action = "StatsResult.php" method = "post">
+<form name = "myForm" method = "post">
 <div>
 <font size = "4">Select sport: </font>
 <select name = "sport" id = "sport1" onChange = "setOptions(document.myForm.sport.options[document.myForm.sport.selectedIndex].value, document.myForm.playerType); ajaxFunction2(); ajaxFunction();">
@@ -256,8 +303,9 @@ function ajaxFunction(){
 <div id = "ajaxDiv2">Statistics will be loaded here</div>
 <div>
 <div id='ajaxDiv'>Date ranges will be loaded here</div>
-<input type= "submit" style = "color: green" value="Compute"></input>
+<input type= "button" style = "color:green" value="Compute" onclick="ajaxStats();"></input>
 </form>
 </body>
 </html>
+
 
