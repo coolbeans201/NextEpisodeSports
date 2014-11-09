1810c701f4fe9fd83e1885358e7f39a45207bbb0
Matthew Weingarten <matthewben5@aol.com>
2014-11-08 23:53:48 -0500

    Update StatsQuery.php

--- StatsQuery.php
+++ StatsQuery.php
@@ -256,6 +256,95 @@ function ajaxStats(){
 		 queryString += "&endYear=" + endYear;
 		 ajaxRequest.open("GET", "getStats.php" + queryString, true);
 		 ajaxRequest.send(null);
+}
+</script>
+<script type = "text/javascript">
+function ajaxFunction(){
+		 var ajaxRequest;  // The variable that makes Ajax possible!
+			
+		 try{
+		   // Opera 8.0+, Firefox, Safari
+		   ajaxRequest = new XMLHttpRequest();
+		 }catch (e){
+		   // Internet Explorer Browsers
+		   try{
+			  ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+		   }catch (e) {
+			  try{
+				 ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+			  }catch (e){
+				 // Something went wrong
+				 alert("Your browser broke!");
+				 return false;
+			  }
+		   }
+		 }
+		 // Create a function that will receive data 
+		 // sent from the server and will update
+		 // div section in the same page.
+		 ajaxRequest.onreadystatechange = function(){
+		   if(ajaxRequest.readyState == 4){
+			  var ajaxDisplay = document.getElementById('ajaxDiv');
+			  ajaxDisplay.innerHTML = ajaxRequest.responseText;
+		   }
+		 }
+		 // Now get the value from user and pass it to
+		 // server script.
+		 var sport = document.getElementById('sport1').value;
+		 var playertype = document.getElementById('playerType1').value;
+		 var queryString = "?sport=" + sport ;
+		 queryString += "&playertype=" + playertype;
+		 ajaxRequest.open("GET", "getYears.php" + queryString, true);
+		 ajaxRequest.send(null); 
+}
+</script>
+<script type = "text/javascript">
+function ajaxStatsLifetimeRetrieval(){
+		 var ajaxRequest;  // The variable that makes Ajax possible!
+			
+		 try{
+		   // Opera 8.0+, Firefox, Safari
+		   ajaxRequest = new XMLHttpRequest();
+		 }catch (e){
+		   // Internet Explorer Browsers
+		   try{
+			  ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP");
+		   }catch (e) {
+			  try{
+				 ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP");
+			  }catch (e){
+				 // Something went wrong
+				 alert("Your browser broke!");
+				 return false;
+			  }
+		   }
+		 }
+		 // Create a function that will receive data 
+		 // sent from the server and will update
+		 // div section in the same page.
+		 ajaxRequest.onreadystatechange = function(){
+		   if(ajaxRequest.readyState == 4){
+			  var ajaxDisplay = document.getElementById('ResultDiv');
+			  ajaxDisplay.innerHTML = ajaxRequest.responseText;
+		   }
+		 }
+		 // Now get the value from user and pass it to
+		 // server script.
+		 var sport = document.getElementById('sport1').value;
+		 var playertype = document.getElementById('playerType1').value;
+		 var operation = document.getElementById('operation1').value;
+		 var stats = document.getElementById('stats').value;
+		 var begYear = document.getElementById('year1').value;
+		 var endYear = document.getElementById('year2').value;
+		 var queryString = "?sport=" + sport;
+		 queryString += "&playertype=" + playertype;
+		 queryString += "&operation=" + operation;
+		 queryString += "&stats=" + stats;
+		 queryString += "&begYear=" + begYear;
+		 queryString += "&endYear=" + endYear;
+		 ajaxRequest.open("GET", "getStatsLifetimeRetrieval.php" + queryString, true);
+		 ajaxRequest.send(null);
+}
 </script>
 </head>
 <body background="squared_metal.png">
@@ -303,10 +392,10 @@ function ajaxStats(){
 <div id = "ajaxDiv2">Statistics will be loaded here</div>
 <div>
 <div id='ajaxDiv'>Date ranges will be loaded here</div>
-<input type= "button" style = "color:green" value="Compute" onclick="ajaxStats();"></input>
+<input type= "button" style = "color:green" value="Compute Per Year" onclick="ajaxStats();"></input>
+<input type= "button" style = "color:blue" value="Compute Lifetime" onclick="ajaxStatsLifetimeRetrieval();"></input>
 </form>
 <div id = "ResultDiv"></div>
 </body>
 </html>
-
 
