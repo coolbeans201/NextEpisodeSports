acd61eae35864a3683dc5b9d5eb132d4bf454536
xin-he <xinhe@ufl.edu>
2014-11-21 18:48:38 -0500

    Update HomePage.php

--- HomePage.php
+++ HomePage.php
@@ -1,4 +1,102 @@
-
+#!/usr/local/bin/php
+<html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+<head>
+	<title>Next Episode</title>
+	<meta name="keywords" content="Next Episode, homepage">
+	<meta name="description" content="Next Episode Sports Database">
+	<meta name="author" content="Derek Hua, Matt Weingarten, Xin He, Jesse Chau">
+	<meta name="copyright" content="Copyright &copy 2014, All Rights Reserved">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light|Rock+Salt">
+	
+    <style>
+        <style style="text/css">
+        html {overflow-y: scroll}
+        
+		h1 {
+			font-family: 'Rock Salt', sans-serif;
+			font-size: 60px;
+			font-weight: normal;
+			color:#000000;
+			text-align: center;
+		}
+		
+		body {
+			font-family: 'Open Sans', sans-serif;
+			font-size: 24px;
+			background-color: #F9F4E1;
+			background : transparent url("http://i.imgur.com/kLuX2Vb.jpg") no-repeat 0px 110px scroll;
+     	}
+		
+		.chooseoperation {
+			text-align:center;
+		}
+		
+        .buttoncenter {
+            text-align:center;
+        }
+        
+        
+		nav ul ul {
+			display: none;
+		}
+		nav ul li:hover > ul {
+			display: block;
+		}
+		
+		nav ul {
+			background: #efefef; 
+			background: linear-gradient(top, #efefef 0%, #bbbbbb 100%);  
+			background: -moz-linear-gradient(top, #efefef 0%, #bbbbbb 100%); 
+			background: -webkit-linear-gradient(top, #efefef 0%,#bbbbbb 100%); 
+			box-shadow: 0px 0px 9px rgba(0,0,0,0.15);
+			padding: 0 20px;
+			border-radius: 10px;  
+			list-style: none;
+			position: relative;
+			display: inline-table;
+		}
+		nav ul:after {
+			content: ""; clear: both; display: block;
+		}
+		
+		nav ul li {
+			float: left;
+		}
+		nav ul li:hover {
+			background: #4b545f;
+			background: linear-gradient(top, #4f5964 0%, #5f6975 40%);
+			background: -moz-linear-gradient(top, #4f5964 0%, #5f6975 40%);
+			background: -webkit-linear-gradient(top, #4f5964 0%,#5f6975 40%);
+		}
+		
+		nav ul li:hover a {
+			color: #fff;
+		}
+		
+		nav ul li a {
+			display: block; padding: 25px 40px;
+			color: #757575; text-decoration: none;
+		}
+		
+		nav ul ul {
+			background: #5f6975; border-radius: 0px; padding: 0;
+			position: absolute; top: 100%;
+		}
+		nav ul ul li {
+			float: none; 
+			border-top: 1px solid #6b727c;
+			border-bottom: 1px solid #575f6a;
+			position: relative;
+		}
+		
+		nav ul ul li a {
+			padding: 15px 40px;
+			color: #fff;
+		}	
+		
+		nav ul ul li a:hover {
+			background: #4b545f;
 		}
 		
 		nav ul ul ul {
@@ -39,3 +137,4 @@
 	</ul>
 </nav>
 </body></html>
+
