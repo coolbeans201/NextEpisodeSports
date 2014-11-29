c9d2edf1e3c5e4d5f0404a3aa79d3d5588fde40e
Matthew Weingarten <matthewben5@aol.com>
2014-11-29 17:28:43 -0500

    Update getTuples.php

--- getTuples.php
+++ getTuples.php
@@ -6,7 +6,27 @@
 	if (!$connection) {
 		die('Could not connect');
 	}
-	$query = "";
+	$query = "SELECT a.aCount + b.bCount + c.cCount + d.dCount+ e.eCount + f.fCount + g.gCount + h.hCount + i.iCount + j.jCount + k.kCount + l.lCount + m.mCount + n.nCount + o.oCount + p.pCount + q.qCount + r.rCount + s.sCount + t.tCount
+					 from(select COUNT(*) aCount from BasketballCoaches) a, 
+					 (select count(*) bCount from basketballplayers) b,
+					 (select count(*) cCount from basketballplayerallstar) c,					 
+					 (select count(*) dCount from basketballteams) d,
+					 (select count(*) eCount from hockeycoaches) e,
+					 (select count (*) fCount from hockeygoalies) f, 
+					 (select count(*) gCount from hockeygoaliesshootout) g,
+					 (select count(*) hCount from hockeyscoring) h,
+					 (select count(*) iCount from hockeyscoringshootout) i, 
+					 (select count(*) jCount from hockeyteams) j,
+					 (select count(*) kCount from hockeyteamspostseason) k, 
+					 (select count(*) lCount from hockeyteamssplit) l, 
+					 (select count(*) mCount from baseballmanagers) m,
+					 (select count(*) nCount from baseballpitching) n,
+					 (select count(*) oCount from baseballpitchingpostseason) o,
+					 (select count(*) pCount from baseballbatting) p,
+					 (select count(*) qCount from baseballbattingpostseason) q,
+					 (select count(*) rCount from baseballfielding) r,
+					 (select count(*) sCount from baseballfieldingpostseason) s,
+					 (select count(*) tCount from baseballteams) t";
 	echo '<font size = "4">';
 	echo "<b> Tuple Count </b> <br>";
 	$statement = oci_parse($connection, $query);
