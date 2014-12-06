edd0fe4dbd403929549368c4fa4725ef34123e2f
Derek Hua <dhua20@yahoo.com>
2014-12-06 01:12:51 -0500

    Update StatsRetrievalQuery.php

--- StatsRetrievalQuery.php
+++ StatsRetrievalQuery.php
@@ -128,22 +128,45 @@
         
 		h1 {
 			font-family: 'Rock Salt', sans-serif;
-			font-size: 60px;
+			font-size: 26px;
 			font-weight: normal;
-			color:#000000;
+			color:#FFFFFF;
 			text-align: center;
+			text-shadow: 2px 2px #000000;
+			padding-bottom: 15px;
 		}
+		
+		body {
+			font-size: 16px;
+			background-color: #F9F4E1;
+			background-image: url("black-gradient-background.png");
+			background-size: 100% 100%;
+			background-repeat: no-repeat;
+     	}
+		
+		.wrapper {
+			width: 100%;
+			overflow: hidden;
+		}
+		.container {
+			width: 100%;
+			margin: 0 auto;
+		}
+		
+		.white{
+			color:#FFFFFF
+		}
+		.banner-img {
+			width: 100%;
+		}
+		
 		#footer {
     			position : absolute;
     			bottom : 0;
     			height : 40px;
     			align : left;
   		}
-		body {
-			font-family: 'Open Sans', sans-serif;
-			font-size: 20px;
-			background-color: #F9F4E1;
-     	}
+		
 		
 		.chooseoperation {
 			text-align:center;
@@ -151,6 +174,7 @@
 		
         .buttoncenter {
             text-align:center;
+			font-size: 24px;
         }
         
         
@@ -230,7 +254,16 @@
 		}
     </style>
 </head>
-<body background="squared_metal.png">
+<body>
+
+<div id="banner">
+    <div id="wrapper">
+        <div id="container">
+                <img class="banner-img" src="NESBanner.png" alt="N.A.L.A. Apparel"/>
+        </div>
+    </div>
+</div>
+
 <h1>Stats Retrieval Query</h1>
 <hr>
 <nav class="buttoncenter">
@@ -250,25 +283,27 @@
 	</ul>
 </nav>
 
-<form name="myform" method="post">
-<div>
-<font size = "4">Select sport:</font>
-<select name="sport" id = "sport" onchange="setOptions(document.myform.sport.options[document.myform.sport.selectedIndex].value, document.myform.playertype); ajaxFunction();">
-    <option value="a">-Select a Sport-</option>
-    <option value="Baseball">Baseball</option>
-    <option value="Basketball">Basketball</option>
-    <option value="Hockey">Hockey</option>
-</select>
+<div class = "white">
+	<form name="myform" method="post">
+	<div>
+	<font size = "4">Select sport:</font>
+	<select name="sport" id = "sport" onchange="setOptions(document.myform.sport.options[document.myform.sport.selectedIndex].value, document.myform.playertype); ajaxFunction();">
+		<option value="a">-Select a Sport-</option>
+		<option value="Baseball">Baseball</option>
+		<option value="Basketball">Basketball</option>
+		<option value="Hockey">Hockey</option>
+	</select>
+	</div>
+	<div>
+	<font size = "4">Select player type:</font>
+	<select name="playertype" id = "playertype" onchange = "ajaxFunction();"></select>
+	</div>
+	<div id='ajaxDiv'>Names will be loaded here</div>
+	<div id = 'buttonDiv'>&nbsp;
+	<input type= "button" class="btn btn-success" style = "color:white" value="Compute" onclick="ajaxStatsRetrieval();"></input> <!--Button-->
+	</div>
+	</form>
+	<div id='textBoxDiv'></div>
 </div>
-<div>
-<font size = "4">Select player type:</font>
-<select name="playertype" id = "playertype" onchange = "ajaxFunction();"></select>
-</div>
-<div id='ajaxDiv'>Names will be loaded here</div>
-<div id = 'buttonDiv'>&nbsp;
-<input type= "button" class="btn btn-success" style = "color:white" value="Compute" onclick="ajaxStatsRetrieval();"></input> <!--Button-->
-</div>
-</form>
-<div id='textBoxDiv'></div>
 </body>
 </html>
