a0a5eb319ebf0d07e73a8cc3e36e4534db1164f2
Derek Hua <dhua20@yahoo.com>
2014-12-06 01:13:06 -0500

    Update HomePage.php

--- HomePage.php
+++ HomePage.php
@@ -7,7 +7,7 @@
 	<meta name="description" content="Next Episode Sports Database">
 	<meta name="author" content="Derek Hua, Matt Weingarten, Xin He, Jesse Chau">
 	<meta name="copyright" content="Copyright &copy 2014, All Rights Reserved">
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light|Rock+Salt">
+	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light|Rock+Salt|Stalinist+One">
 	
     <style>
         <style style="text/css">
@@ -15,24 +15,40 @@
         
 		h1 {
 			font-family: 'Rock Salt', sans-serif;
-			font-size: 60px;
+			font-size: 26px;
 			font-weight: normal;
-			color:#000000;
+			color:#FFFFFF;
 			text-align: center;
+			text-shadow: 2px 2px #000000;
+			padding-bottom: 15px;
 		}
 		
 		body {
-			font-family: 'Open Sans', sans-serif;
-			font-size: 24px;
+			font-size: 36px;
 			background-color: #F9F4E1;
-			background : transparent url("http://i.imgur.com/kLuX2Vb.jpg") no-repeat 0px 110px scroll;
+			background-image: url("black-gradient-background.png");
+			background-size: 100% 100%;
+			background-repeat: no-repeat;
      	}
+		
+		.wrapper {
+			width: 100%;
+			overflow: hidden;
+		}
+		.container {
+			width: 100%;
+			margin: 0 auto;
+		}
+		.banner-img {
+			width: 100%;
+		}
 		
 		.chooseoperation {
 			text-align:center;
 		}
 		
         .buttoncenter {
+			margin-top:10px;
             text-align:center;
         }
         
@@ -43,7 +59,6 @@
 		nav ul li:hover > ul {
 			display: block;
 		}
-		
 		nav ul {
 			background: #efefef; 
 			background: linear-gradient(top, #efefef 0%, #bbbbbb 100%);  
@@ -89,37 +104,66 @@
 			border-bottom: 1px solid #575f6a;
 			position: relative;
 		}
-		
 		nav ul ul li a {
 			padding: 15px 40px;
 			color: #fff;
 		}	
-		
 		nav ul ul li a:hover {
 			background: #4b545f;
 		}
-		
 		nav ul ul ul {
 			position: absolute; left: 100%; top:0;
 		}
 		
-		hr{
-			border: 0;
-			color: #9E9E9E;
-			background-color: #9E9E9E;
-			height: 5px;
-			width: 100%;
-			text-align: left;
+		
+		#summary{
+			display:block;
+			padding:0;
+			border:100px;
+			text-align:center;
+			font-size:18px;
 		}
+		
+		p {
+			margin-right: 100px;
+			margin-left: 100px;
+			margin-bottom: 20px;
+			align: center;
+			color:#FFFFFF;
+		}
+		
+		p.groove {border-style: groove;}
+		
     </style>
 
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(function() {
+		$('.fadein img:gt(0)').hide();
+
+		setInterval(function () {
+			$('.fadein :first-child').fadeOut()
+									 .next('img')
+									 .fadeIn()
+									 .end()
+									 .appendTo('.fadein');
+		}, 7000); // 7 seconds
+		});
+	</script>
+	
 </head>
 
-<body background="squared_metal.png">
+<body>
 
-<h1> Welcome to Next Episode Sports! </h1>
+<div id="banner">
+    <div id="wrapper">
+        <div id="container">
+                <img class="banner-img" src="NESBanner.png" alt="N.A.L.A. Apparel"/>
+        </div>
+    </div>
+</div>
 
-<hr>
 
 <nav class="buttoncenter">
 	<ul>
@@ -136,5 +180,42 @@
 		<li><a href="Inspiration.php">Inspiration</a></li>
 	</ul>
 </nav>
+
+<hr>
+
+<!--
+	<div class="fadein">
+		<img src="http://icdn2.digitaltrends.com/image/6efb2224e7a20942e784a29d6e9ad1e2-4136x1424.jpg?ver=1" size="1920px 500px">
+		<img src="horseheadnebula.jpg">
+	</div>
+	
+-->
+
+<div id ="summary">
+	<table align="center">
+			<tr>	<!-- table row -->
+				<td> <div><h1>Welcome to Next Episode Sports!</h1></div> </td>		<!-- table column/data -->
+			</tr>	
+				
+			<tr>
+				<td> 
+					<div id=summary>
+						<p>This databases project will create a webpage that will contain a comprehensive sports database, </p>
+						<p>covering basketball, baseball, and hockey. The webpage will allow the user to retrieve season-by-season </p>
+						<p>and career stats for players, coaches, and teams, as well as see how many standard deviations a player, </p>
+						<p>coach, or team falls below or above the mean in a certain statistic. The webpage will also allow the user </p>
+						<p>to sort players, coaches, or teams by a certain statistic, as well as compare two or more players, coaches, </p>
+						<p>or teams on the sum of their standard deviations above or below the mean across all their statistics.</p>
+						<br/>
+						<hr>
+					</div>
+				</td>		
+				
+			</tr>
+			
+	</table>
+</div>
+
 </body></html>
+
 
