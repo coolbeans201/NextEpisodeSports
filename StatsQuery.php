cd19dccb16ded2ce350efa13e51222145a1211d0
JChauster <chaudown5@hotmail.com>
2014-12-06 01:22:15 -0500

    table

--- StatsQuery.php
+++ StatsQuery.php
@@ -10,6 +10,17 @@
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light|Rock+Salt">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
         <style style="text/css">
+        
+        .datagrid table { border-collapse: collapse; text-align: left; width: 100%; } 
+		.datagrid {
+					
+					font: Arial, Helvetica, sans-serif; 
+					background: #fff; overflow: hidden; border: 10px solid #36752D; 
+					-webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px; 
+					}
+		.datagrid table td, .datagrid table th { padding: 3px 10px; }
+		.datagrid table thead th {background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #36752D), color-stop(1, #275420) );background:-moz-linear-gradient( center top, #36752D 5%, #275420 100% );filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#36752D', endColorstr='#275420');background-color:#36752D; color:#FFFFFF; font-size: 15px; font-weight: bold; border-left: 1px solid #36752D; } .datagrid table thead th:first-child { border: none; }.datagrid table tbody td { color: #275420; border-left: 1px solid #C6FFC2;font-size: 20px;font-weight: normal; }.datagrid table tbody .alt td { background: #DFFFDE; color: #275420; }.datagrid table tbody td:first-child { border-left: none; }.datagrid table tbody tr:last-child td { border-bottom: none; }.datagrid table tfoot td div { border-top: 1px solid #36752D;background: #DFFFDE;} .datagrid table tfoot td { padding: 0; font-size: 18px } .datagrid table tfoot td div{ padding: 2px; }.datagrid table tfoot td ul { margin: 0; padding:0; list-style: none; text-align: right; }.datagrid table tfoot  li { display: inline; }.datagrid table tfoot li a { text-decoration: none; display: inline-block;  padding: 2px 8px; margin: 1px;color: #FFFFFF;border: 10px solid #36752D;-webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px; background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #36752D), color-stop(1, #275420) );background:-moz-linear-gradient( center top, #36752D 5%, #275420 100% );filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#36752D', endColorstr='#275420');background-color:#36752D; }.datagrid table tfoot ul.active, .datagrid table tfoot ul a:hover { text-decoration: none;border-color: #275420; color: #FFFFFF; background: none; background-color:#36752D;}div.dhtmlx_window_active, div.dhx_modal_cover_dv { position: fixed !important; }
+        
         html {overflow-y: scroll}
         
 		h1 {
@@ -435,7 +446,9 @@ function ajaxStatsLifetimeRetrieval(){
 	<input type= "button" class="btn btn-primary" style = "color:white" value="Compute Lifetime" onclick="ajaxStatsLifetimeRetrieval();"></input>
 	</div>
 	</form>
+	<nav class="datagrid ">
 	<div id = "ResultDiv"></div>
+	</nav>
 <div/>
 </body>
 </html>
