b1319ae33ad3ecb440a7846f69bab85d20460f9e
Matthew Weingarten <matthewben5@aol.com>
2014-12-06 11:15:23 -0500

    Update StatsRetrievalQuery.php

--- StatsRetrievalQuery.php
+++ StatsRetrievalQuery.php
@@ -13,7 +13,7 @@
 	<script type="text/javascript">
 	function setOptions(chosen, selBox){
 	selBox.options.length = 0;
-	if (chosen == "selectasport")
+	if (chosen == "a")
 	{
 	}
 	if (chosen == "Baseball")
