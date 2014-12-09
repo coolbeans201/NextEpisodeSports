eb10af2c16242628c9526e3ae3030f9894a9a3c7
xin-he <xinhe@ufl.edu>
2014-12-09 18:39:06 -0500

    Update getTuples.php

--- getTuples.php
+++ getTuples.php
@@ -27,18 +27,18 @@
 					 (select count(*) rCount from baseballfielding) r,
 					 (select count(*) sCount from baseballfieldingpostseason) s,
 					 (select count(*) tCount from baseballteams) t";
-	echo '<font size = "4">';
+	echo '<font size = "4" color = "white">';
 	echo "<b> Tuple Count </b> <br>";
 	$statement = oci_parse($connection, $query);
 	oci_execute($statement);
 	echo "<table border='1'>\n";
 	echo "<tr>\n";
-	echo '<th>Total Tuples</th>';
+	echo '<th><font color = "white">Total Tuples</th>';
 	echo '</tr>';
 	while ($row = oci_fetch_array($statement, OCI_ASSOC+OCI_RETURN_NULLS)) {
 			
 				foreach ($row as $item) {
-					echo "    <td>" . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
+					echo '    <td><font color = "white">' . ($item !== null ? htmlentities($item, ENT_QUOTES) : "&nbsp;") . "</td>\n";
 				}
 				echo "</tr>\n";
 			}
