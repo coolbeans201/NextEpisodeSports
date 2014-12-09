ee8a0d2d009269f714a373d8e92551676591d242
xin-he <xinhe@ufl.edu>
2014-12-09 17:38:31 -0500

    Update Team.php

--- Team.php
+++ Team.php
@@ -10,30 +10,70 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
     <style>
         <style style="text/css">
+	  .datagrid table { border-collapse: collapse; text-align: left; width: 100%; } 
+	.datagrid {
+					
+					font: Arial, Helvetica, sans-serif; 
+					background: #fff; overflow: hidden; border: 10px solid #36752D; 
+					-webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px; 
+					}
+		.datagrid table td, .datagrid table th { padding: 3px 10px; }
+		.datagrid table thead th {background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #36752D), color-stop(1, #275420) );background:-moz-linear-gradient( center top, #36752D 5%, #275420 100% );filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#36752D', endColorstr='#275420');background-color:#36752D; color:#FFFFFF; font-size: 15px; font-weight: bold; border-left: 1px solid #36752D; } .datagrid table thead th:first-child { border: none; }.datagrid table tbody td { color: #275420; border-left: 1px solid #C6FFC2;font-size: 20px;font-weight: normal; }.datagrid table tbody .alt td { background: #DFFFDE; color: #275420; }.datagrid table tbody td:first-child { border-left: none; }.datagrid table tbody tr:last-child td { border-bottom: none; }.datagrid table tfoot td div { border-top: 1px solid #36752D;background: #DFFFDE;} .datagrid table tfoot td { padding: 0; font-size: 18px } .datagrid table tfoot td div{ padding: 2px; }.datagrid table tfoot td ul { margin: 0; padding:0; list-style: none; text-align: right; }.datagrid table tfoot  li { display: inline; }.datagrid table tfoot li a { text-decoration: none; display: inline-block;  padding: 2px 8px; margin: 1px;color: #FFFFFF;border: 10px solid #36752D;-webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px; background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #36752D), color-stop(1, #275420) );background:-moz-linear-gradient( center top, #36752D 5%, #275420 100% );filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#36752D', endColorstr='#275420');background-color:#36752D; }.datagrid table tfoot ul.active, .datagrid table tfoot ul a:hover { text-decoration: none;border-color: #275420; color: #FFFFFF; background: none; background-color:#36752D;}div.dhtmlx_window_active, div.dhx_modal_cover_dv { position: fixed !important; }
+    
+    
+    
         html {overflow-y: scroll}
         
 		h1 {
 			font-family: 'Rock Salt', sans-serif;
-			font-size: 60px;
+			font-size: 26px;
 			font-weight: normal;
-			color:#000000;
+			color:#FFFFFF;
 			text-align: center;
+			text-shadow: 2px 2px #000000;
+			padding-bottom: 15px;
 		}
 		
 		body {
-			font-family: 'Open Sans', sans-serif;
+			font-size: 16px;
 			background-color: #F9F4E1;
+			background-image: url("black-gradient-background.png");
+			background-size: 100% 100%;
+			background-repeat: no-repeat;
      	}
 		
-		.chooseoperation {
-			text-align:center;
+		.wrapper {
+			width: 100%;
+			overflow: hidden;
+		}
+		.container {
+			width: 100%;
+			margin: 0 auto;
 		}
 		
-        .buttoncenter {
+		.white{
+			color:#FFFFFF
+		}
+		
+		.black{
+			color:#000000
+		}
+		
+		.query{
+			background: #efefef; 
+			background: linear-gradient(top, #efefef 0%, #bbbbbb 100%);  
+			background: -moz-linear-gradient(top, #efefef 0%, #bbbbbb 100%); 
+			background: -webkit-linear-gradient(top, #efefef 0%,#bbbbbb 100%); 
+		}
+		
+		.banner-img {
+			width: 100%;
+		}
+		
+		 .buttoncenter {
             text-align:center;
 			font-size: 24px;
         }
-        
         
 		nav ul ul {
 			display: none;
@@ -101,12 +141,6 @@
 			position: absolute; left: 100%; top:0;
 		}
 		
-		#main {
-			margin-left: 250px;
-			margin-right: 250px;
-			font-family: 'Shadows Into Light', sans-serif;
-			font-size: 36px;
-		}
 		hr{
 			border: 0;
 			color: #9E9E9E;
@@ -119,6 +153,14 @@
 
 </head>
 <body>
+	<div id="banner">
+    <div id="wrapper">
+        <div id="container">
+                <img class="banner-img" src="NESBanner.png" alt="N.A.L.A. Apparel"/>
+        </div>
+    </div>
+	</div>
+	
 <h1>Team</h1>
 <hr>
 <nav class="buttoncenter">
@@ -144,19 +186,19 @@
 			<col span="1" style="width: 25%;">
 		</colgroup>
 		<tr> 
-			<td> <p><b><u>Jesse Chau</u></b><br/>Jesse is a third-year Computer Science and Engineering major with a minor in Physics from Boca Raton, FL. In his free time, he likes to edit photos and play League of Legends. He hopes to land an internship for the summer.</p> </td>
+			<td> <p style="color:white"><b><u>Jesse Chau</u></b><br/>Jesse is a third-year Computer Science and Engineering major with a minor in Physics from Boca Raton, FL. In his free time, he likes to edit photos and play League of Legends. He hopes to land an internship for the summer.</p> </td>
 			<td> <img src="https://scontent-b-mia.xx.fbcdn.net/hphotos-xfa1/v/t1.0-9/p417x417/393080_10151642856640833_1186289759_n.jpg?oh=226b96a499931729a658068c4dd3f6fe&oe=550D5EA5" height = "100" /> </td>
 		</tr>	
 		<tr> 
-			<td> <p><b><u>Xin He</u></b><br/>Xin is a third-year Computer Science major with a minor in Business Administration from Boca Raton, FL. In his free time, Xin likes to watch movies, browse the Internet, and play Planetside 2, a computer game he has dedicated much of his time to. He hopes to land an internship for the summer and beyond.</p></td>
+			<td> <p style="color:white"><b><u>Xin He</u></b><br/>Xin is a third-year Computer Science major with a minor in Business Administration from Boca Raton, FL. In his free time, Xin likes to watch movies, browse the Internet, and play Planetside 2, a computer game he has dedicated much of his time to. He hopes to land an internship for the summer and beyond.</p></td>
 			<td> <img src="https://fbcdn-sphotos-d-a.akamaihd.net/hphotos-ak-xpf1/v/t1.0-9/304095_2413614714151_327036085_n.jpg?oh=751118a2b7f9fd35c471e1327713e4ac&oe=54F47EA7&__gda__=1423248366_b69ff1e3a905b0174196f83c7390a541" height = "100" /> </td>
 		</tr>
 		<tr> 
-			<td> <p><b><u>Derek Hua</u></b><br/>Derek is a third-year Computer Science and Engineering major with a minor in ... from Boca Raton, FL. He is currently seeking opportunities to apply himself in the form of a technical Co-Op or an internship. He not only seeking opportunities that will test my technical knowledge, but ones that will also encourage me to step out of my comfort zone, actively learn, and connect with others.</p> </td>
+			<td> <p style="color:white"><b><u>Derek Hua</u></b><br/>Derek is a third-year Computer Science and Engineering major with a minor in ... from Boca Raton, FL. He is currently seeking opportunities to apply himself in the form of a technical Co-Op or an internship. He not only seeking opportunities that will test my technical knowledge, but ones that will also encourage me to step out of my comfort zone, actively learn, and connect with others.</p> </td>
 			<td> <img src="https://scontent-a-mia.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/p417x417/10433079_401682496648066_6183619574339687075_n.jpg?oh=936862b69bef7511e5e9c1cc075e9072&oe=54FBC559" height = "100"/></td>
 		</tr>
 		<tr> 
-			<td> <p><b><u>Matt Weingarten</u></b><br/>Matt is a third-year Computer Science major with minors in Mathematics and Statistics from Safety Harbor, FL. In his free time, he likes to watch and play sports (hence, the inspiration for this website), play video games, and play Bridge. He never regrets being a Computer Science major, even though the projects that it entails can be time-consuming.</p> </td>
+			<td> <p style="color:white"><b><u>Matt Weingarten</u></b><br/>Matt is a third-year Computer Science major with minors in Mathematics and Statistics from Safety Harbor, FL. In his free time, he likes to watch and play sports (hence, the inspiration for this website), play video games, and play Bridge. He never regrets being a Computer Science major, even though the projects that it entails can be time-consuming.</p> </td>
 			<td> <img src="https://scontent-b-mia.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/1920334_10203996137276308_8992998182298037988_n.jpg?oh=703adef206b1629659d03624303816d7&oe=551A3C78" height = "100" /> </td>
 		</tr>
 	</table>
