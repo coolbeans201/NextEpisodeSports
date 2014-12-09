f87b5dc4222f1227630bf8a01bf23a88d2261675
xin-he <xinhe@ufl.edu>
2014-12-09 17:38:11 -0500

    Update Inspiration.php

--- Inspiration.php
+++ Inspiration.php
@@ -9,32 +9,72 @@
 	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans|Shadows+Into+Light|Rock+Salt">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 	
-    <style>
+<style>
         <style style="text/css">
+	  .datagrid table { border-collapse: collapse; text-align: left; width: 100%; } 
+	.datagrid {
+					
+					font: Arial, Helvetica, sans-serif; 
+					background: #fff; overflow: hidden; border: 10px solid #36752D; 
+					-webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px; 
+					}
+		.datagrid table td, .datagrid table th { padding: 3px 10px; }
+		.datagrid table thead th {background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #36752D), color-stop(1, #275420) );background:-moz-linear-gradient( center top, #36752D 5%, #275420 100% );filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#36752D', endColorstr='#275420');background-color:#36752D; color:#FFFFFF; font-size: 15px; font-weight: bold; border-left: 1px solid #36752D; } .datagrid table thead th:first-child { border: none; }.datagrid table tbody td { color: #275420; border-left: 1px solid #C6FFC2;font-size: 20px;font-weight: normal; }.datagrid table tbody .alt td { background: #DFFFDE; color: #275420; }.datagrid table tbody td:first-child { border-left: none; }.datagrid table tbody tr:last-child td { border-bottom: none; }.datagrid table tfoot td div { border-top: 1px solid #36752D;background: #DFFFDE;} .datagrid table tfoot td { padding: 0; font-size: 18px } .datagrid table tfoot td div{ padding: 2px; }.datagrid table tfoot td ul { margin: 0; padding:0; list-style: none; text-align: right; }.datagrid table tfoot  li { display: inline; }.datagrid table tfoot li a { text-decoration: none; display: inline-block;  padding: 2px 8px; margin: 1px;color: #FFFFFF;border: 10px solid #36752D;-webkit-border-radius: 10px; -moz-border-radius: 10px; border-radius: 10px; background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #36752D), color-stop(1, #275420) );background:-moz-linear-gradient( center top, #36752D 5%, #275420 100% );filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#36752D', endColorstr='#275420');background-color:#36752D; }.datagrid table tfoot ul.active, .datagrid table tfoot ul a:hover { text-decoration: none;border-color: #275420; color: #FFFFFF; background: none; background-color:#36752D;}div.dhtmlx_window_active, div.dhx_modal_cover_dv { position: fixed !important; }
+    
+    
+    
         html {overflow-y: scroll}
         
 		h1 {
 			font-family: 'Rock Salt', sans-serif;
-			font-size: 60px;
+			font-size: 26px;
 			font-weight: normal;
-			color:#000000;
+			color:#FFFFFF;
 			text-align: center;
+			text-shadow: 2px 2px #000000;
+			padding-bottom: 15px;
 		}
 		
 		body {
-			font-family: 'Open Sans', sans-serif;
-			font-size: 24px;
+			font-size: 16px;
 			background-color: #F9F4E1;
+			background-image: url("black-gradient-background.png");
+			background-size: 100% 100%;
+			background-repeat: no-repeat;
      	}
 		
-		.chooseoperation {
-			text-align:center;
+		.wrapper {
+			width: 100%;
+			overflow: hidden;
+		}
+		.container {
+			width: 100%;
+			margin: 0 auto;
 		}
 		
-        .buttoncenter {
+		.white{
+			color:#FFFFFF
+		}
+		
+		.black{
+			color:#000000
+		}
+		
+		.query{
+			background: #efefef; 
+			background: linear-gradient(top, #efefef 0%, #bbbbbb 100%);  
+			background: -moz-linear-gradient(top, #efefef 0%, #bbbbbb 100%); 
+			background: -webkit-linear-gradient(top, #efefef 0%,#bbbbbb 100%); 
+		}
+		
+		.banner-img {
+			width: 100%;
+		}
+		
+		 .buttoncenter {
             text-align:center;
+			font-size: 24px;
         }
-        
         
 		nav ul ul {
 			display: none;
@@ -111,7 +151,7 @@
 			text-align: left;
 		}
     </style>
-
+	
 </head>
 <script type = "text/javascript">
 function ajaxFunction(){
@@ -151,6 +191,15 @@ function ajaxFunction(){
 	}
 </script>
 <body background="squared_metal.png">
+
+	<div id="banner">
+    <div id="wrapper">
+        <div id="container">
+                <img class="banner-img" src="NESBanner.png" alt="N.A.L.A. Apparel"/>
+        </div>
+    </div>
+	</div>
+	
 <h1>Inspiration</h1>
 <hr>
 <nav class="buttoncenter">
@@ -170,7 +219,7 @@ function ajaxFunction(){
 		</ul>
 	</nav>
 <form name="myform" method="post">
-<p>The Internet contains many databases covering each sport, such as basketball-reference.com, hockey-reference.com, or baseball-reference.com. However, there are no comprehensive sports database webpages out there. Since it seems more useful for all the sports information a user could want to lie in one website rather than a bunch of different websites, we combined the stats for teams, players, and coaches from baseball, basketball, and hockey into this database. We hope you enjoy!</p>
+<p style="color:white">The Internet contains many databases covering each sport, such as basketball-reference.com, hockey-reference.com, or baseball-reference.com. However, there are no comprehensive sports database webpages out there. Since it seems more useful for all the sports information a user could want to lie in one website rather than a bunch of different websites, we combined the stats for teams, players, and coaches from baseball, basketball, and hockey into this database. We hope you enjoy!</p>
 <div id = 'buttonDiv'>&nbsp;
 <input type= "button" class="btn btn-success" style = "color:white" value="Calculate Tuples" onclick="ajaxFunction();"></input> <!--Button-->
 <div id='resultDiv'></div>
